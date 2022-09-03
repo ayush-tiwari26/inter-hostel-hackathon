ae13e7e75bb9a09e80c0c060e1ab63b6168e27e4
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-09-03 17:26:44 +0000

    fix port issue

---
+++ backend/__tests__/student.test.js
@@ -0,0 +1,12 @@
+// const { request } = require('supertest');
+
+// const { app } = require('../app');
+
+// describe('Student Login', () => {
+//     it('should login a student', async () => {
+//         const response = await request(app)
+//             .post('/api/student/login')
+//             .send({
+//                 email: 'ce20b112@smail.iitm.ac.in'
+//             });
+// })
